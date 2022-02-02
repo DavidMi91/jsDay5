ef8b1eb8e1488f4d0f738efdb4fc1fb05f37375d
David Michalitsch <david.michalitsch@gmail.com>
2022-02-02 10:26:44 +0100

    minor changes

---
+++ David/Basic/script.js
@@ -0,0 +1,10 @@
+// let person = {}
+
+
+
+
+// let array = [person, person1, person2]     
+
+// for (let i=0, i < Array.length; i++) {
+//     document.getElementById("result").innerHTML += array[index].info();
+// }
